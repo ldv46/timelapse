4d2e111310e7b57a7eeaafff566917232c72091b
ldv46 <r.b.vandergaag@hotmail.com>
2014-08-26 22:51:59 +0200

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,20 +1,14 @@
-# First test in image capture
-
 import time
 import picamera
 
+file_path = '/home/pi/timelapse/foto/' # Where to save the pictures
+
 with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    camera.framerate = 30
-    # Give the camera's auto-exposure and auto-white-balance algorithms
-    # some time to measure the scene and determine appropriate values
-    camera.ISO = 200
-    time.sleep(2)
-    # Now fix the values
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g
-    # Finally, take several photos with the fixed settings
-    camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+    camera.rotation = 180 # Only if camera module is not upright
+    camera.resolution = (2592, 1944) # Maximum resolution because bigger is better in post
+    camera.ISO = 100 # 100, 200, 320, 400, 500, 640, 800
+    camera.shutter_speed = 333333 # shutterspeed of the camera, 1000000 = 1 second
+    camera.awb_mode = 'horizon' # off, auto, sunlight, cloudy, shade, tungsten, fluorescent, incandescent, flash, horizon
+    for filename in camera.capture_continuous(file_path + 'timelapse_{timestamp:%Y%m%d%H%M%S}.jpg'): # prefix with timestamp (date, month, day, hour, minute, seccond) to ensure unique filenames
+        print('Captured %s' % filename)
+        time.sleep(1) # time between shots in seconds
