3d3a95c7aaea10c154ef06858e2b0e8da612dcb7
ldv46 <r.b.vandergaag@hotmail.com>
2014-09-01 22:10:37 +0200

    1st continuous test code

--- timelapse.py
+++ timelapse.py
@@ -4,11 +4,13 @@ import picamera
 file_path = '/home/pi/timelapse/foto/' # Where to save the pictures
 
 with picamera.PiCamera() as camera:
+    camera.led = False # turn off camera led
+    camera.start_preview()
     camera.rotation = 180 # Only if camera module is not upright
     camera.resolution = (2592, 1944) # Maximum resolution because bigger is better in post
     camera.ISO = 100 # 100, 200, 320, 400, 500, 640, 800
-    camera.shutter_speed = 333333 # shutterspeed of the camera, 1000000 = 1 second
+    camera.shutter_speed = 333333 # shutterspeed of the camera, 1000000 = 1 second 333333 = 1/3 second
     camera.awb_mode = 'horizon' # off, auto, sunlight, cloudy, shade, tungsten, fluorescent, incandescent, flash, horizon
     for filename in camera.capture_continuous(file_path + 'timelapse_{timestamp:%Y%m%d%H%M%S}.jpg'): # prefix with timestamp (date, month, day, hour, minute, seccond) to ensure unique filenames
         print('Captured %s' % filename)
-        time.sleep(1) # time between shots in seconds
+        time.sleep(20) # time between shots in seconds
